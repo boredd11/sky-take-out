29b4bdde096d4bda3957baaf581161e333d847f8
zhoumo <123@qq.com>
2025-08-31 16:16:23 +0800

    用户下单功能代码开发

---
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -0,0 +1,33 @@
+package com.sky.controller.user;
+
+import com.sky.dto.OrdersSubmitDTO;
+import com.sky.result.Result;
+import com.sky.service.OrderService;
+import com.sky.vo.OrderSubmitVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("userOrderController")
+@RequestMapping("/user/order")
+@Slf4j
+@Api("C端用户下单相关接口")
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    /**
+     * 用户下单
+     * @param ordersSubmitDTO
+     * @return
+     */
+    @PostMapping("/submit")
+    @ApiOperation("用户下单")
+    public Result<OrderSubmitVO> save(@RequestBody OrdersSubmitDTO ordersSubmitDTO){
+        log.info("用户下单信息：{}",ordersSubmitDTO);
+        OrderSubmitVO orderSubmitVO = orderService.OrderSubmit(ordersSubmitDTO);
+        return Result.success(orderSubmitVO);
+    }
+}
